e01918920f35d6549a3f2b406bce3d007c903d8d
samascogh <samsolomon363@gmail.com>
2023-04-24 06:50:46 -0700

    creating builtins.c, update header.h

---
+++ builtins.c
@@ -0,0 +1,27 @@
+#include "header.h"
+#include <stdlib.h>
+/**
+ * funct_exit - function to implement exit function
+ * @args: args[0] command
+ * Return: 0
+ */
+int funct_exit(char **args)
+{
+	(void) **args;
+	exit(0);
+	return (0);
+}
+/**
+ * funct_env - function to print the current environment
+ * @args: args[0] command
+ * Return: 0
+ */
+int funct_env(char **args)
+{
+	int idx;
+
+	(void) **args;
+		for (idx = 0; environ[idx]; idx++)
+			printf("%s\n", environ[idx]);
+	return (0);
+}
